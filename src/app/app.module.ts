8807675edadd1fba0866a129595bbccca1bb8e51
Victoriia-Ripka <vikaripka@gmail.com>
2023-11-10 13:19:27 +0100

    added router, new components, navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 // рут модуль корневий компонент, потрібний щоб застосунок запустився
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,9 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
 import { HelloWorldListComponent } from './hello-world-list/hello-world-list.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
 
 // декоратор визначає як модуль повинен правюувати
 @NgModule({ 
@@ -19,16 +23,27 @@ import { HelloWorldListComponent } from './hello-world-list/hello-world-list.com
     HeaderComponent,
     FooterComponent,
     MainComponent,
-    HelloWorldListComponent
+    HelloWorldListComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component
   ],
-  // інші модулі, які необхідні для роботи додатка
+  // компоненти, що будуть видимі для інших модулів
+  exports: [],
+  // компоненти, що необхідні для роботи цього модуля
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '/', component: AppComponent},
+      {path: '/hello-world', component: HelloWorldListComponent},
+      {path: '/something', component: AppComponent},
+      {path: '', redirectTo: "/", pathMatch: "full"}
+    ])
   ],
-  // для кореневого модуля
+  // сервіси, які даний модуль додає до всіх сервісів додатка 
   providers: [],
-  // компонент,з якого починається робота нашого додатка
+  // визначає кореневий компонент
   bootstrap: [AppComponent]
 })
 export class AppModule { }
