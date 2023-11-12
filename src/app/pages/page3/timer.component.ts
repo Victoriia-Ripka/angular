89f6441dc40a8cfe40c70bf40fdb146e501f7b79
Victoriia-Ripka <vikaripka@gmail.com>
2023-11-12 17:09:35 +0100

    timer done

--- src/app/pages/page3/timer.component.ts
+++ src/app/pages/page3/timer.component.ts
@@ -6,14 +6,29 @@ import { Component, EventEmitter, Input, Output } from '@angular/core'
     styleUrls: ["timer.component.css"]
 })
 export class TimerComponent {
+    private intervalObject: any
+    private currentValue: number = 0
+
+    @Input()
     interval: number = 1000
 
+    @Output()
+    tick: EventEmitter<number> = new EventEmitter()
 
     onStart() {
-
+        if(this.intervalObject) return
+        this.intervalObject = setInterval(() => this.callback(), this.interval)
     }
 
     onStop() {
+        if(!this.intervalObject) return
+        clearInterval(this.intervalObject)
+        this.intervalObject = false
+    }
 
+    // розібратися як emit в компоненті і EventEmitter пов'язані з event i timerHandler на сторінці
+    private callback() {
+        this.currentValue += 1
+        this.tick.emit(this.currentValue)
     }
 }
