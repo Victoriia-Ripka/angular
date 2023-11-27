2abb0abbf4eabe58c805c0f2b62718d6d82658ea
Victoriia-Ripka <vikaripka@gmail.com>
2023-11-27 12:06:13 +0100

    added message compontnts

---
+++ src/app/components/message/message.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'message',
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.css']
+})
+export class MessageComponent {
+  visible: boolean = true
+
+  hide(): void {
+    this.visible = false
+  }
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core"
 import { CommonModule } from "@angular/common"
 
-import { HelloWorldListComponent, CardComponent } from "../components/index"
+import { HelloWorldListComponent, CardComponent, MessageComponent } from "../components/index"
 import { Page1Component, Page2Component, Page3Component, ChapterComponent, Counter, TimerComponent } from "./index"
 import {MatCardModule} from '@angular/material/card';
 
@@ -9,7 +9,8 @@ import {MatCardModule} from '@angular/material/card';
     imports: [CommonModule, MatCardModule],
     declarations: [Page1Component, Page2Component, Page3Component, 
         HelloWorldListComponent, CardComponent, TimerComponent, 
-        ChapterComponent, Counter],
+        ChapterComponent, Counter, MessageComponent
+    ],
     exports: [Page1Component, Page2Component, Page3Component]
 })
 export class PagesModule { }
