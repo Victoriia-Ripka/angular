5f420452f34ac67b62a09a67ca12c166532602b3
Victoriia-Ripka <vikaripka@gmail.com>
2023-11-28 13:20:52 +0100

    added new rout - page4

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from "@angular/core"
 import { CommonModule } from "@angular/common"
 
 import { HelloWorldListComponent, CardComponent, MessageComponent } from "../components/index"
-import { Page1Component, Page2Component, Page3Component, ChapterComponent, Counter, TimerComponent } from "./index"
+import { Page1Component, Page2Component, Page3Component, Page4Component, ChapterComponent, Counter, TimerComponent } from "./index"
 import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
     imports: [CommonModule, MatCardModule],
-    declarations: [Page1Component, Page2Component, Page3Component, 
+    declarations: [Page1Component, Page2Component, Page3Component, Page4Component, 
         HelloWorldListComponent, CardComponent, TimerComponent, 
         ChapterComponent, Counter, MessageComponent
     ],
-    exports: [Page1Component, Page2Component, Page3Component]
+    exports: [Page1Component, Page2Component, Page3Component, Page4Component]
 })
 export class PagesModule { }
--- src/app/pages/pages.routs.ts
+++ src/app/pages/pages.routs.ts
@@ -1,7 +1,8 @@
-import { Page1Component, Page2Component, Page3Component } from "./index";
+import { Page1Component, Page2Component, Page3Component, Page4Component } from "./index";
 
 export const routs = [
     { path: "page1", component: Page1Component},
     { path: "page2", component: Page2Component},
-    { path: "page3", component: Page3Component }
+    { path: "page3", component: Page3Component },
+    { path: "page4", component: Page4Component }
 ]
